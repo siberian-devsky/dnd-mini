1b4d2f55472eafb937ea26b4941bc16e133e4356
Jeff Martin <chess.logic@gmail.com>
2025-10-12 16:24:02 -0700

    rewrite express to serve client files as well

--- server/app.js
+++ server/app.js
@@ -1,50 +1,23 @@
-import express from "express";
-import cors from "cors";
-import prisma from "./dbClient.js";
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 8000;
+const PORT = 3000;
 
-app.use(cors());
-app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.get("/heartbeat", (req, res) => {
-    res.status(200).send({ message: "no complaints here" });
-});
+app.use(cors())
+app.use(express.json())
 
-// Fetch a player by ID or by role.
-app.get("/player/:id", (req, res) => {});
+app.use(express.static(path.join(__dirname, "../client")));
 
-// new player
-app.post("/player", async (req, res) => {
-    const { type } = req.body;
-    try {
-        await prisma.player.create({
-            data: { type, wins: 0, losses: 0 },
-        });
-        const players = await prisma.player.findMany();
-        res.json(players);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Failed to create player" });
-    }
-});
+app.get('/api/ping', (req, res) => {
+    res.status(200).send({ message: "pong" })
+})
 
-// Create a new fight with given players + maxRounds.
-app.post("/fight", async (req, res) => {
-    const fightData = await req.body;
-    const players = fightData.players;
-    const maxRounds = fightData.rounds;
-});
-
-// Fetch fight + all rounds + winner.
-app.get("/fight/:id", (req, res) => {});
-
-// Add a round result (heroRoll, villainRoll, winner) to a fight.
-app.post("/round", async (req, res) => {
-    const roundData = await req.body;
-    const heroRoll = roundData.heroRoll;
-    const villainRoll = roundData.villainRoll;
-});
-
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(PORT, () => {
+    console.log(`server listening on localhost:${PORT}`)
+})
--- server/prisma/migrations/20251002173656_add_name_col_to_player_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Player` ADD COLUMN `name` VARCHAR(191) NOT NULL DEFAULT 'boonanner';
