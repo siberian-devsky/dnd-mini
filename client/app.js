f4de65942835b06818dc9778b168cf59a9f4fece
Jeff Martin <chess.logic@gmail.com>
2025-10-13 10:32:35 -0700

    Add dice locks
    * simple toggle button + logic to lock/unlock dice

--- client/app.js
+++ client/app.js
@@ -2,27 +2,32 @@ const dice = document.querySelectorAll(".die");
 
 function rollDie() {
     dice.forEach((die) => {
-        const x = 90 * Math.floor(Math.random() * 8); // 0, 90, 180, 270
+        const x = 90 * Math.floor(Math.random() * 8);
         const y = 90 * Math.floor(Math.random() * 8);
-        die.style.transform = `rotateX(${x}deg) rotateY(${y}deg) scale3d(.5, .5, .5)`;
-        setTimeout(() => {
-            die.style.transform = `rotateX(${x}deg) rotateY(${y}deg) scale3d(1, 1, 1)`;
-        }, 500)
+
+        const isLocked = die.dataset.locked === "true";
+
+        if (!isLocked) {
+            die.style.transform = `rotateX(${x}deg) rotateY(${y}deg) scale3d(.5, .5, .5)`;
+            setTimeout(() => {
+                die.style.transform = `rotateX(${x}deg) rotateY(${y}deg) scale3d(1, 1, 1)`;
+            }, 500);
+        }
     });
 }
 
 async function loadFaces() {
     // grab the template and stringify
-    const response = await fetch("./face.svg")
-    const svgTemplate = await response.text()
+    const response = await fetch("./face.svg");
+    const svgTemplate = await response.text();
 
     // convert to DOM
-    const parser = new DOMParser()
-    const parsedSvg = parser.parseFromString(svgTemplate, "image/svg+xml")
-    const svgTag = parsedSvg.documentElement
+    const parser = new DOMParser();
+    const parsedSvg = parser.parseFromString(svgTemplate, "image/svg+xml");
+    const svgTag = parsedSvg.documentElement;
 
     // get the target faces for each die
-    const faces = document.querySelectorAll(".face")
+    const faces = document.querySelectorAll(".face");
 
     // pip combinations - map on a 9x9 grid
     pipCombos = {
@@ -31,34 +36,36 @@ async function loadFaces() {
         3: [1, 5, 9],
         4: [1, 3, 7, 9],
         5: [1, 3, 5, 7, 9],
-        6: [1, 3, 4, 6, 7, 9]
-    }
+        6: [1, 3, 4, 6, 7, 9],
+    };
 
     faces.forEach((face) => {
         // clone the svgElement and add a child
-        const clone = svgTag.cloneNode(true)
-        face.appendChild(clone)
+        const clone = svgTag.cloneNode(true);
+        face.appendChild(clone);
 
-        const faceValue = parseInt(face.dataset.value)
-        const pipMap = pipCombos[faceValue]
-        
+        const faceValue = parseInt(face.dataset.value);
+        const pipMap = pipCombos[faceValue];
+
         // svg is now available for use
-        const circles = clone.querySelectorAll("circle[data-pos]")
+        const circles = clone.querySelectorAll("circle[data-pos]");
         circles.forEach((circle) => {
-            const pos = parseInt(circle.dataset.pos)
+            const pos = parseInt(circle.dataset.pos);
 
             if (pipMap.includes(pos)) {
-                circle.setAttribute("fill", "black")
+                circle.setAttribute("fill", "black");
             } else {
-                circle.setAttribute("fill", "none")
+                circle.setAttribute("fill", "none");
             }
-        })
-    })
+        });
+    });
 }
-    
+
 function toggleInstructions() {
-    const instructionsEl = document.querySelector("#instructionsModalContainer");
-	const cssVars = window.getComputedStyle(instructionsEl);
+    const instructionsEl = document.querySelector(
+        "#instructionsModalContainer"
+    );
+    const cssVars = window.getComputedStyle(instructionsEl);
     const isVisible = cssVars.visibility === "visible" ? true : false;
 
     if (isVisible) {
@@ -68,4 +75,23 @@ function toggleInstructions() {
     }
 }
 
-loadFaces()
+function lockDie(buttonId, dieId) {
+    const targetDie = document.querySelector(dieId);
+    const lockButton = document.querySelector(`#${buttonId}`);
+
+    const isLocked = targetDie.dataset.locked === "true";
+
+    if (isLocked) {
+        // unlock
+        targetDie.dataset.locked = "false";
+        lockButton.style.background = null;
+        lockButton.textContent = "Lock";
+    } else {
+        // lock
+        targetDie.dataset.locked = "true";
+        lockButton.style.background = "rgba(255, 0, 0, 0.75)";
+        lockButton.textContent = "Locked";
+    }
+}
+
+loadFaces();
