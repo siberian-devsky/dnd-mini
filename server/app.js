d4b2450725ad49d68d7c74b2dd387e995172870d
Jeff Martin <chess.logic@gmail.com>
2025-10-12 17:26:10 -0700

    add a simple server

--- server/app.js
+++ server/app.js
@@ -15,7 +15,39 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, "../client")));
 
 app.get('/api/ping', (req, res) => {
-    res.status(200).send({ message: "pong" })
+    res.json({ message: "pong" })
+})
+
+app.post("/api/parrot", (req, res) => {
+    const data = req.body
+    if (!data) {
+        res
+        .status(400)
+        .json({
+            message: 'no data to parrot',
+            timestamp: new Date().toDateString(),
+            timestamp: new Date().toLocaleDateString(),
+            timestamp: new Date().toLocaleString(),
+            timestamp: new Date().toLocaleTimeString(),
+            timestamp: new Date().toTimeString(),
+            timestamp: new Date().toUTCString()
+        })
+    }
+
+    try {
+        res
+        .set({
+            'X-Data-Authority': 'Goldwater',
+            'X-Ownwer': 'zilch'
+        })
+        .json({
+            message: "data parroting",
+            body: {data: data, 'new data': 42},
+            date: new Date().toISOString()
+        })
+    } catch (err) {
+        res.status(500).json( {message: `generic error ${err}`} )
+    }
 })
 
 app.listen(PORT, () => {
